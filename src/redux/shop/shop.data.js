97312e323096e90fc7f62d687135902c4c1133d1
Busel Alexandr <buseloff@gmail.com>
2022-10-02 10:26:38 +0300

    Green shop site, frontend done

---
+++ src/redux/shop/shop.data.js
@@ -0,0 +1,249 @@
+const SHOP_DATA = {
+  berries: {
+    id: 1,
+    title: 'Berries',
+    routeName: 'berries',
+    items: [
+      {
+        id: 1,
+        name: 'Blueberry',
+        imageUrl: require('../../assets/img/berries/blueberries.jpg'),
+        price: 8
+      },
+      {
+        id: 2,
+        name: 'Strawberry',
+        imageUrl: require('../../assets/img/berries/strawberries.jpg'),
+        price: 5
+      },
+      {
+        id: 3,
+        name: 'Raspberry',
+        imageUrl: require('../../assets/img/berries/raspberries.jpg'),
+        price: 7
+      },
+      {
+        id: 4,
+        name: 'Blackberry',
+        imageUrl: require('../../assets/img/berries/blackberries.jpg'),
+        price: 6
+      },
+      {
+        id: 5,
+        name: 'Red Currants',
+        imageUrl: require('../../assets/img/berries/redcurrants.jpg'),
+        price: 8
+      },
+      {
+        id: 6,
+        name: 'White Currants',
+        imageUrl: require('../../assets/img/berries/whitecurrants.jpg'),
+        price: 9
+      },
+      {
+        id: 7,
+        name: 'Blackcurrants',
+        imageUrl: require('../../assets/img/berries/blackcurrants.jpg'),
+        price: 8
+      },
+      {
+        id: 8,
+        name: 'Cranberry',
+        imageUrl: require('../../assets/img/berries/cranberries.jpg'),
+        price: 12
+      },
+      {
+        id: 9,
+        name: 'Gooseberry',
+        imageUrl: require('../../assets/img/berries/gooseberry.jpg'),
+        price: 10
+      }
+    ]
+  },
+  vegetables: {
+    id: 2,
+    title: 'Vegetables',
+    routeName: 'vegetables',
+    items: [
+      {
+        id: 10,
+        name: 'Brussels Sprouts',
+        imageUrl: require('../../assets/img/vegetables/brusselssprouts.jpg'),
+        price: 10
+      },
+      {
+        id: 11,
+        name: 'Cucumber',
+        imageUrl: require('../../assets/img/vegetables/cucumber.jpg'),
+        price: 3
+      },
+      {
+        id: 12,
+        name: 'Zucchini',
+        imageUrl: require('../../assets/img/vegetables/zucchini.jpg'),
+        price: 6.5
+      },
+      {
+        id: 13,
+        name: 'Potato',
+        imageUrl: require('../../assets/img/vegetables/potatoes.jpg'),
+        price: 2
+      },
+      {
+        id: 14,
+        name: 'Celery',
+        imageUrl: require('../../assets/img/vegetables/celery.jpg'),
+        price: 10
+      },
+      {
+        id: 15,
+        name: 'Onion',
+        imageUrl: require('../../assets/img/vegetables/onions.jpg'),
+        price: 3
+      },
+      {
+        id: 16,
+        name: 'Garlic',
+        imageUrl: require('../../assets/img/vegetables/garlic.jpg'),
+        price: 10
+      },
+      {
+        id: 17,
+        name: 'Tomato',
+        imageUrl: require('../../assets/img/vegetables/tomatoes.jpg'),
+        price: 4.5
+      }
+    ]
+  },
+  fruits: {
+    id: 3,
+    title: 'Fruits',
+    routeName: 'fruits',
+    items: [
+      {
+        id: 18,
+        name: 'Orange',
+        imageUrl: require('../../assets/img/fruits/oranges.jpg'),
+        price: 7
+      },
+      {
+        id: 19,
+        name: 'Grapefruit',
+        imageUrl: require('../../assets/img/fruits/grapefruits.jpg'),
+        price: 12
+      },
+      {
+        id: 20,
+        name: 'Nectarine',
+        imageUrl: require('../../assets/img/fruits/nectarines.jpg'),
+        price: 11
+      },
+      {
+        id: 21,
+        name: 'Mango',
+        imageUrl: require('../../assets/img/fruits/mangoes.jpg'),
+        price: 15
+      },
+      {
+        id: 22,
+        name: 'Plum',
+        imageUrl: require('../../assets/img/fruits/plums.jpg'),
+        price: 4
+      }
+    ]
+  },
+  cereals: {
+    id: 4,
+    title: 'Cereals',
+    routeName: 'cereals',
+    items: [
+      {
+        id: 23,
+        name: 'Rice',
+        imageUrl: require('../../assets/img/cereals/rice.jpg'),
+        price: 3.75
+      },
+      {
+        id: 24,
+        name: 'Corn',
+        imageUrl: require('../../assets/img/cereals/corn.jpg'),
+        price: 8
+      },
+      {
+        id: 25,
+        name: 'Oats',
+        imageUrl: require('../../assets/img/cereals/oats.jpg'),
+        price: 5
+      },
+      {
+        id: 26,
+        name: 'Sorghum',
+        imageUrl: require('../../assets/img/cereals/sorghum.jpg'),
+        price: 12
+      },
+      {
+        id: 27,
+        name: 'Quinoa',
+        imageUrl: require('../../assets/img/cereals/quinoa.jpg'),
+        price: 12
+      },
+      {
+        id: 28,
+        name: 'Millet',
+        imageUrl: require('../../assets/img/cereals/millet.jpg'),
+        price: 9
+      },
+      {
+        id: 29,
+        name: 'Buckwheat',
+        imageUrl: require('../../assets/img/cereals/buckwheat.jpg'),
+        price: 6
+      }
+    ]
+  },
+  spices: {
+    id: 5,
+    title: 'Spices',
+    routeName: 'spices',
+    items: [
+      {
+        id: 30,
+        name: 'Cardamom',
+        imageUrl: require('../../assets/img/spices/cardamom.jpg'),
+        price: 15
+      },
+      {
+        id: 31,
+        name: 'Cinnamon',
+        imageUrl: require('../../assets/img/spices/cinnamon.jpg'),
+        price: 20
+      },
+      {
+        id: 32,
+        name: 'Cloves',
+        imageUrl: require('../../assets/img/spices/cloves.jpg'),
+        price: 25
+      },
+      {
+        id: 33,
+        name: 'Turmeric',
+        imageUrl: require('../../assets/img/spices/turmeric.jpg'),
+        price: 25
+      },
+      {
+        id: 34,
+        name: 'Saffron',
+        imageUrl: require('../../assets/img/spices/saffron.jpg'),
+        price: 20
+      },
+      {
+        id: 35,
+        name: 'Cumin',
+        imageUrl: require('../../assets/img/spices/cumin.jpg'),
+        price: 25
+      }
+    ]
+  }
+};
+
+export default SHOP_DATA;
